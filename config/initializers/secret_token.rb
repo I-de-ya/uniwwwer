c0a035de55fb7bdfc28720a1c2e94d7a718647f7
inner-whisper <maks.rydkin@gmail.com>
2011-11-08 21:05:14 +0400

    HelloWorld

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uniwwwer::Application.config.secret_token = '681ca54a9b74a075fb7d403b2ad644739910bd93f71162f500856a7427b946deab7edff6c44d3cdffcd83a89e4a199d10915a81e4c96aa7a3d933d71aebdea2f'
